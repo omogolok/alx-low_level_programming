5673f25b50e35998328ab688939f7cb61a7cf7f4
Omogolo Tshiamo K. Likius <omogolotshiamolikius@gmail.com>
2023-02-23 10:10:45 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints string followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
